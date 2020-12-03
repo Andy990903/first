d84d4aec96d958c194f45e00708c5c10f59b6d0e
zt-zmp <2339734115@qq.com>
2020-12-03 11:50:17 +0800

    修改后

---
+++ client/src/components/Banner.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="banner">
+        <div class="hot-tags">
+            热门标签
+        </div>
+        <div class="hot-posts">
+            点击排行
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Banner"
+    }
+</script>
+
+<style scoped>
+    .hot-tags, .hot-posts {
+        height: 300px;
+        border: 1px solid blue;
+        padding: 2em;
+    }
+</style>
